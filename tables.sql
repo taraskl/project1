cd3b51ab02663d5ffefc8618e6f5397bedef96b3
Taras <taras.kliuchnyk@gmail.com>
2019-06-05 18:45:33 +0300

    Delete tables.sql

--- tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE reviews (
-    id SERIAL PRIMARY KEY,
-    rating INTEGER NOT NULL,
-    review TEXT NOT NULL,
-    user_id INTEGER REFERENCES users,
-    book_id INTEGER REFERENCES books
-);
